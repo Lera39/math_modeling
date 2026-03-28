4cb74960913b0b43bd1d4a1ea4da88d0ceae17ad
Lera39 <elka55715@gmail.com>
2026-03-28 12:25:27 +0000

    что-то непонятное

--- test_2.py
+++ test_2.py
@@ -4,7 +4,7 @@ import random
 from matplotlib.animation import FuncAnimation
 
 class Person:
-    def __init__(self, x, y, speed_x, speed_y, contacts, kof_ver, time_i, time_r, chance = False, is_infected = False):   
+    def __init__(self, x, y, speed_x, speed_y, contacts, kof_ver, time_i, time_r, time_e, incub = False, chance = False, is_infected = False):   
         self.x = x
         self.y = y
         self.speed_x = speed_x * np.cos(angel)
@@ -13,6 +13,7 @@ class Person:
         self.is_infected = is_infected
         self.time_i = time_i
         self.time_r = time_r
+        self.incub = incub
         self.contacts = contacts
         self.kof_ver = kof_ver
         
@@ -36,6 +37,10 @@ class Person:
         return False
 
 
+    def incub_period(self):
+        self.incub = True
+
+
     def infect(self):
             self.is_infected = True
 
@@ -46,6 +51,12 @@ class Person:
                 self.kof_ver += 0.0005
                 self.contacts += 1
         return self.kof_ver, self.contacts
+
+
+    def lost_e(self, persons):
+        if self.incub == True and self.incub_period >=1:
+            self.is_infected = True
+            self.time_e = 0
 
 
     def lost_i(self):
@@ -70,6 +81,7 @@ for i in range (k):
     angel = random.random() * 2 * np.pi
     time_i = 0
     time_r = 0
+    time_e = 0
     contacts = 0
 
     v = random.random()
@@ -95,6 +107,8 @@ for i in range (k):
         random.uniform(0.4, 0.6),   #kof_ver
         time_i,
         time_r,
+        time_e,
+        incub,
         chance,
         is_infected,
     ))
@@ -104,8 +118,8 @@ figure, axises = plt.subplots()
 animation_red_points, = plt.plot([], [], 'o', color = 'red')
 animation_green_points, = plt.plot([], [], 'o', color = 'green')
 animation_blue_points, = plt.plot([], [], 'o', color = 'blue')
+animation_orange_points, = plt.plot([],[], 'o', color = 'orange')
 
-# title_obj = axises.set_title(f'S = {S}, I = {I}, R = {R}, time = 0')
 
 def update(frame):
     data_red_points_x = []
@@ -114,6 +128,8 @@ def update(frame):
     data_green_points_y = []
     data_blue_points_x = []
     data_blue_points_y = []
+    data_orange_points_x = []
+    data_orange_points_y = []
 
     # S = len(data_green_points_x)
     # I = len(data_red_points_x)
@@ -129,10 +145,13 @@ def update(frame):
         elif person.chance == True:
             person.time_r += 0.00045
             person.lost_r()
+        elif person.incub_period == True:
+            person.time_e += 0.01
+            person.lost_e()
 
         person.change_contacts(persons)
         if person.near_infect(persons):
-            person.infect()
+            person.incube_period()
  
         if person.is_infected:
             data_red_points_x.append(person.x)
@@ -140,6 +159,9 @@ def update(frame):
         elif person.chance:
             data_blue_points_x.append(person.x)
             data_blue_points_y.append(person.y)
+        elif person.incub:
+            data_orange_points_x.append(person.x)
+            data_orange_points_y.append(person.y)
         else:
             data_green_points_x.append(person.x)
             data_green_points_y.append(person.y)
@@ -147,16 +169,20 @@ def update(frame):
     animation_red_points.set_data(data_red_points_x, data_red_points_y)
     animation_green_points.set_data(data_green_points_x, data_green_points_y)
     animation_blue_points.set_data(data_blue_points_x, data_blue_points_y)
+    animation_orange_points.set_data(data_orange_points_x, data_orange_points_y)
 
     # β = (len(data_green_points_x) / time) * k / len(data_red_points_x) / len(data_green_points_x) * (-1)   # коэффициент заражения
     # γ = len(data_blue_points_x) / (time * len(data_red_points_x))   # коэффициент выздоровления
 
-    return animation_red_points, animation_green_points, animation_blue_points
+    return animation_red_points, animation_green_points, animation_blue_points, animation_orange_points
 
 
 axises.set_xlim(-5, 5)
 axises.set_ylim(-5, 5)
+
 frames = np.arange(1000)
+#1 period bolezni = 250 frames; 1 day = 50 frames при условии, что болезнь протекает у всех одинаково
+# инкубационный период 2 дня = 100 frames; +0,01
 animation = FuncAnimation(figure, update, frames=frames, interval=50)
 animation.save('virus.gif')
 plt.show()
